9201e677814fc68e31adca718884fd44f1a4a18e
ReformedMrozu <56279952+ReformedMrozu@users.noreply.github.com>
2019-11-06 01:27:28 +0100

    fixes 06/11/2019

--- Lab 4/z1.cpp
+++ Lab 4/z1.cpp
@@ -1,61 +1,61 @@
-//#include <iostream>
-//#include <stdlib.h>
-//#include <stdio.h>
-//using namespace std;
-//
-//class Vector{
-//private:
-//	//private variables
-//	int x;
-//	int y;
-//public:
-//	Vector(){//constructor
-//		/*'this' wskazuje na adres pamiêci obecnego obiektu (klasa object) */
-//		this->x = 0;
-//		this->y = 0;	
-//	}
-//	void show(void){
-//		printf("\n %d , %d\n",this->x,this->y);
-//	}
-//	void get_data(){
-//		printf("\nEnter X: ");
-//		cin>>x;
-//		printf("\nEnter y: ");
-//		cin>>y;
-//	}
-//	///////////////// HANDLE OPERANDS ///////////////////
-//	Vector(double x, double y)
-//	{
-//		this->x = x;
-//		this->y = y;
-//	}
-//	Vector operator+ (const Vector &vec){
-//		return 
-//			Vector(this->x +vec.x,this->y+vec.y);
-//	}
-//	Vector operator- (const Vector &vec){
-//		return 
-//			Vector(this->x -vec.x,this->y-vec.y);
-//	}
-//};
-//
-//int main(void){
-//
-//	Vector A,B,Sum;
-//
-//	A.get_data();
-//	B.get_data();
-//	
-//	Sum= A+B;
-//	cout<<"\nDodawanie: ";
-//	Sum.show();
-//
-//	Sum= A-B;
-//	cout<<"\nOdejmowanie: ";
-//	Sum.show();
-//	cout<<"\n\n";
-//	
-//
-//
-//	return true;
-//}
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+class Vector{
+private:
+	//private variables
+	int x;
+	int y;
+public:
+	Vector(){//constructor
+		/*'this' wskazuje na adres pamiêci obecnego obiektu (klasa object) */
+		this->x = 0;
+		this->y = 0;	
+	}
+	void show(void){
+		printf("\n %d , %d\n",this->x,this->y);
+	}
+	void get_data(){
+		printf("\nEnter X: ");
+		cin>>x;
+		printf("\nEnter y: ");
+		cin>>y;
+	}
+	///////////////// HANDLE OPERANDS ///////////////////
+	Vector(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+	Vector operator+ (const Vector &vec){
+		return 
+			Vector(this->x +vec.x,this->y+vec.y);
+	}
+	Vector operator- (const Vector &vec){
+		return 
+			Vector(this->x -vec.x,this->y-vec.y);
+	}
+};
+
+int main(void){
+
+	Vector A,B,Sum;
+
+	A.get_data();
+	B.get_data();
+	
+	Sum= A+B;
+	cout<<"\nDodawanie: ";
+	Sum.show();
+
+	Sum= A-B;
+	cout<<"\nOdejmowanie: ";
+	Sum.show();
+	cout<<"\n\n";
+	
+
+
+	return true;
+}
